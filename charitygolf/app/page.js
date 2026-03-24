1a8b61501a5867b5ab55aa3a82762166e25b88c2
Anoop Chaudhari <anoopchaudhari263@gmail.com>
2026-03-24 14:39:01 +0530

    Add Home component for Golf Charity Platform

---
+++ charitygolf/app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <h1 className="text-3xl font-bold">
+        Welcome to Golf Charity Platform 🚀
+      </h1>
+    </div>
+  );
+}
